6d2258f900ef82ec51494c907499b7452d7c1c3a
Christian Burk <christian@skyriter.com>
2020-10-22 06:51:15 -0500

    adds a way to pass the path in for another pagge

--- html-css/wk07-assignment-exercise-file-FINISHED/town_hall_3/scripts/speakers.js
+++ html-css/wk07-assignment-exercise-file-FINISHED/town_hall_3/scripts/speakers.js
@@ -1,22 +1,24 @@
 // localStorage.setItem(" " {});
 // let value = localStorage.getItem("");
 
-window.addEventListener("DOMContentLoaded", function() {
-    const orderButton = document.querySelector("button.order-link[data-order]");
-    orderButton.addEventListener("click", function(e) {
-        e.preventDefault;
-        const button = e.currentTarget;
-        const container = button.parentNode;
+function getDataForForm(current,destination) {
+    window.addEventListener("DOMContentLoaded", function() {
+        const orderButton = document.querySelector("button.order-link[data-order]");
+        orderButton.addEventListener("click", function(e) {
+            const button = e.currentTarget;
+            const container = button.parentNode;
 
-        const order = {
-            id: button.getAttribute("data-order"),
-            title: container.querySelector("h1").innerText,
-            date: container.querySelector("h2").innerText,
-            speaker: container.querySelector("h3").innerText
-        };
-        localStorage.setItem("order", JSON.stringify(order));
+            const order = {
+                id: button.getAttribute("data-order"),
+                title: container.querySelector("h1").innerText,
+                date: container.querySelector("h2").innerText,
+                speaker: container.querySelector("h3").innerText
+            };
+            localStorage.setItem("order", JSON.stringify(order));
 
-        const url = window.location.href.replace("speakers/toobin.html", "tickets.html");
-        window.location.href = url;
+            
+            const url = window.location.href.replace(current, destination);
+            window.location.href = url;
+        });
     });
-});
+}
